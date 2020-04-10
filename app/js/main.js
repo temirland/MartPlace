6f0060d98947a3be936c1543b79e847d8ef59f37
temirland <rakanov.t@gmail.com>
2020-04-10 12:38:16 +0600

    added followers feed section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
 $(function(){
 
+
+
+    $('.products__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.prev-slide'),
+        nextArrow: $('.next-slide'),
+        
+
+    });
+
+  
     var mixer = mixitup('.products__inner-box');
 });
