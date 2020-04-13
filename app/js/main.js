ac019acdac7b88868b2f762a58082fe8f0d1181b
temirland <rakanov.t@gmail.com>
2020-04-13 11:59:37 +0600

    search bar finished

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,22 @@ $(function(){
 
 
     $('.products__slider').slick({
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: $('.prev-slide'),
         nextArrow: $('.next-slide'),
+        variableWidth: true,
+        variableHeight: true
         
 
     });
 
+
+    // $('.search__drop-down').on('click', function(){
+    //     $(this).children('.drop-down__link').toggleClass('active');
+    //     $(this).children('.drop-down__categories').toggleClass('active');
+    // });
   
     var mixer = mixitup('.products__inner-box');
 });
